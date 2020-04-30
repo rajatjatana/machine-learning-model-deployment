76aca5bd4354565bb44fb3f707060dd2bb89e7a4
*Rajat <*jatana.rajat@gmail.com*>
2020-04-30 11:41:20 -0400

    kaggle script to save the datasets to the regression/dataset

--- scripts/kaggle.sh
+++ scripts/kaggle.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p datasets/
+kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/datasets
