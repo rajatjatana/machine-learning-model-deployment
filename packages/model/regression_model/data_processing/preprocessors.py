ef54c3ddd99b49731b0e0eb8dd92dbe67a074e6c
*Rajat <*jatana.rajat@gmail.com*>
2020-05-03 19:00:02 -0400

    added the categorical and numerical imputer

--- packages/model/regression_model/data_processing/preprocessors.py
+++ packages/model/regression_model/data_processing/preprocessors.py
@@ -1,8 +1,52 @@
 import pandas as pd
 from sklearn.base import BaseEstimator , TransformerMixin
 
+# In this notebook, we will steps for
+class CategoricalImputer(BaseEstimator, TransformerMixin):
+    """Categorical data missing value imputer."""
 
-class CategoricalImputer(BaseEstimator , TransformerMixin) :
-    """ Categorical data missing value imputer"""
+    def __init__(self, variables=None) -> None:
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
 
-    def __init__(selfself , variables=None):
+    def fit(self, X: pd.DataFrame, y: pd.Series = None
+            ) -> 'CategoricalImputer':
+        """Fit statement to accomodate the sklearn pipeline."""
+
+        return self
+
+    def transform(self, X: pd.DataFrame) -> pd.DataFrame:
+        """Apply the transforms to the dataframe."""
+
+        X = X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].fillna('Missing')
+
+        return X
+
+
+
+class NumericalImputer(BaseEstimator, TransformerMixin):
+    """Numerical missing value imputer."""
+
+    def __init__(self, variables=None):
+        if not isinstance(variables, list):
+            self.variables = [variables]
+        else:
+            self.variables = variables
+
+    def fit(self, X, y=None):
+        # persist mode in a dictionary
+        self.imputer_dict_ = {}
+        for feature in self.variables:
+            self.imputer_dict_[feature] = X[feature].mode()[0]
+        return self
+
+    def transform(self, X):
+        X = X.copy()
+        for feature in self.variables:
+            X[feature].fillna(self.imputer_dict_[feature], inplace=True)
+        return X
+
